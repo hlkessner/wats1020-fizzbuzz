5aefa97ffe69963871a7a0fa0a0c8d7ca82811e5
kessnerh <kessnerh@seattlu.edu>
2016-06-26 16:58:20 -0400

    added html and js documents

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -4,9 +4,12 @@
 //Variable for FizzBizz number range 
 var rangelimit = 20;
 //variable for numbers divisible by 3
-var responseFor3= 'Fizz';
+var responseFor3= 'Fizz' ;
 //variable for numbers divisible by 5
 var responseFor5= 'Buzz';
+
+
+
 
 
 //Count through number range to figure out if each number is divisible by 3, 5 or both.
@@ -17,19 +20,19 @@ for (i=1; i<=rangelimit; i++){
     //if so: see if it is also devisible by 5
     if (i % 5 ===0) {
       // If divisible by 3 and 5: print FizzBuzz
-      document.write (responseFor3+responseFor5); 
+      document.write (responseFor3+responseFor5+'<br/ >');
     } else {
       // If only divisible by 3: Print Fizz
-      document.write (responseFor3);
+      document.write (responseFor3+'<br/ >'); 
     }
   } else if (i % 5 === 0) {
      // See if it is divisible by 5
      //if so: Print Buzz
-     document.write (responseFor5);
+     document.write (responseFor5+'<br/ >'); 
    } else {
      //See if it is not divisible by 3 or 5
      // If so: Print Number
-     document.write(i);
+     document.write(i+'<br/ >'); 
    }
 }
 
